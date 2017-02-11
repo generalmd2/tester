7e35c7083a100066d73ef7542259285ef7e82189
generalmd2 <monkeydieluffy2@gmail.com>
2017-02-11 11:46:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,21 @@ const client = new discord.Client;
 const token = "Mjc5OTIyMzQxNDc4MTM3ODU2.C4B5hA.16zG9SKeRGGfBX2YwFswmGm_hlk";
 const prefix = "=";
 var status = prefix + "aide";
-var streamingURL = "http://densetsuno.forumactif.org";
-const Porno = require('porno');
-const prn = new Porno();
 const mention  = "test";
-var Lolibooru = require("node-lolibooru");
-var loli = new Lolibooru();
+
 
 client.on("ready", () => {
 var memberCount = client.users.size;
 var servercount = client.guilds.size;
   var servers = client.guilds.array().map(g => g.name).join(',');
   console.log("--------------------------------------");
-  console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :smiley:  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+  console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :smiley:  \n[!]);
   client.user.setGame(status, streamingURL); //Joue à ...
 });
 
 client.on("message", message => {
-	if (message.content.startsWith(prefix + 'loli')){
-		var lolli = message.content.substr(5);
-
-		loli.getLoli(1,1, /*"1girl"*/lolli).then((data) => {
-			console.log(data[0].preview_url);
-			message.channel.sendMessage(data[0].preview_url);
-		})
+	if (message.content.startsWith(prefix + 'cc')){
+		message.reply("coucou");
 	}
 
 
